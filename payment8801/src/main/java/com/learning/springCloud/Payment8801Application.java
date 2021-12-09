cddc1e0d6495babde22d244275f95548571e0e81
zhumengren <franciszmr@foxmail.com>
2021-12-09 17:46:02 +0800

    Eureka服务注册中心的引入，并将原本提供服务的那一部分注入到服务注册中心中。同时因为SpringBoot版本和Eureka版本不适配的原因，降低SpringBoot的版本为2.5.1，可以成功使用Eureka服务注册中心

--- payment8801/src/main/java/com/learning/springCloud/Payment8801Application.java
+++ payment8801/src/main/java/com/learning/springCloud/Payment8801Application.java
@@ -3,9 +3,11 @@ package com.learning.springCloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.learning.springCloud.mapper")
+@EnableEurekaClient
 public class Payment8801Application {
 
     public static void main(String[] args) {
--- payment8801/src/main/java/com/learning/springCloud/controller/PaymentController.java
+++ payment8801/src/main/java/com/learning/springCloud/controller/PaymentController.java
@@ -23,37 +23,35 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
-    /*
+    /**
      * @Author zhumengren
      * @param payment
      * @Date 2021/12/8 12:46
      * @Description 新建一条数据
-     **/
-    // @RequestBody(required = false)
+     */
     @PostMapping("/create")
     public CommonResult create(@RequestBody Payment payment) {
         Payment payment1 = paymentService.create(payment);
-        log.info("*********插入数据成功！");
-        if (payment1 != null && !payment1.equals("")) {
-            System.out.println("成功！");
+        if (payment1 != null) {
+            log.info("数据插入成功！");
             CommonResult commonResult = new CommonResult(200, "数据插入成功！", payment);
-            System.out.println("commonResult的值为：" + commonResult);
+            log.info("commonResult的值为", commonResult);
             return commonResult;
         } else {
-            System.out.println("失败！");
+            log.info("数据插入失败！");
             return new CommonResult(405, "数据插入失败！");
         }
     }
 
-    /*
+    /**
      * @Author zhumengren
      * @param id
      * @Date 2021/12/8 12:46
      * @Description 通过payment的id查询payment对象
-     **/
+     */
     @GetMapping("/get/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id) {
-//        System.out.println("Controler层执行开始！");
+        log.info("Controler层执行开始");
         Payment paymentById = paymentService.getPaymentById(id);
         if (paymentById != null) {
             return new CommonResult(200, "成功查询到数据！", paymentById);
