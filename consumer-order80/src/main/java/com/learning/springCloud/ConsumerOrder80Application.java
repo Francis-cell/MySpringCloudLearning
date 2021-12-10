33126d251f48dd5c8b2e84538de1a41e2797bd97
zhumengren <franciszmr@foxmail.com>
2021-12-10 10:15:36 +0800

    进行eureka集群环境的搭建，一共两个服务注册中心，eureka7001和eureka7002，并完成测试

--- consumer-order80/src/main/java/com/learning/springCloud/ConsumerOrder80Application.java
+++ consumer-order80/src/main/java/com/learning/springCloud/ConsumerOrder80Application.java
@@ -2,8 +2,10 @@ package com.learning.springCloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsumerOrder80Application {
 
     public static void main(String[] args) {
