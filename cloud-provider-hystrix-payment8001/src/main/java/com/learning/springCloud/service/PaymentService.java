a750e016702555c096c618b79acec2e35b758aec
zhumengren <franciszmr@foxmail.com>
2021-12-16 14:28:59 +0800

    服务熔断操作执行完成

--- cloud-provider-hystrix-payment8001/src/main/java/com/learning/springCloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/learning/springCloud/service/PaymentService.java
@@ -1,8 +1,10 @@
 package com.learning.springCloud.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -37,6 +39,29 @@ public class PaymentService {
     /** 用来兜底的方法（当正常的运行不能执行的时候调用） */
     public String paymentInfo_TimeOutHandler(Integer id) {
         return "线程池："+Thread.currentThread().getName()+"   系统运行超时或者异常，请稍后再试！,id：  "+id+"\t"+ "::>_<::";
+    }
+
+    /** 服务熔断（Start） */
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),  //是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),   //请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),  //时间范围
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60"), //失败率达到多少后跳闸
+    })
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        if (id < 0){
+            throw new RuntimeException("*****id 不能负数");
+        }
+        // 相当于使用了 UUID.randomUUID().toString()
+        // 这个工具要学会使用
+        String serialNumber = IdUtil.simpleUUID();
+
+        return Thread.currentThread().getName()+"\t"+"调用成功,流水号："+serialNumber;
+    }
+
+    /** 服务降级方法 */
+    public String paymentCircuitBreaker_fallback(@PathVariable("id") Integer id){
+        return "id 不能负数，请稍候再试,(┬＿┬)/~~     id: " +id;
     }
 
 }
